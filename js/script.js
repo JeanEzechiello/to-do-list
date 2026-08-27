4bf8bc7495cdc9f2556d7156ec0680cf3a7fd25f
Jean Ezechiello <jeanezechielo2008@gmail.com>
2026-08-27 00:57:53 -0300

    remover e tarefa concluida

--- js/script.js
+++ js/script.js
@@ -8,11 +8,25 @@ addbutton.addEventListener("click", function() {
 
 let novatarefa = document.createElement('li')
 let remove = document.createElement('button')
+let concluido = document.createElement('button')
 
 novatarefa.innerText = txt
 remove.innerText = "Excluir"
+
 novatarefa.appendChild(remove)
 ul.appendChild(novatarefa)
 
+concluido.innerText = "✓"
+novatarefa.appendChild(concluido)
+
 ipt.value = ''
+
+remove.addEventListener('click', function(){
+    novatarefa.remove()
+});
+
+concluido.addEventListener('click', function(){
+    novatarefa.classList.add('concluida')
+});
+
 });
