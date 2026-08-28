0bfaa2252d8bcef225cf93a0d3d8a00fd4736b01
Jean Ezechiello <jeanezechielo2008@gmail.com>
2026-08-27 22:37:48 -0300

    separa lógica de criar tarefa em função própria

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const addbutton = document.getElementById('addbutton')
 const ipt = document.getElementById('input-tarefa')
 const ul = document.getElementById('lista-tarefas')
+let tarefas = []
 
 addbutton.addEventListener("click", function() {
     let txt = ipt.value
+    criarTarefa(txt)
 
+    tarefas.push(txt)
+    localStorage.setItem("trf", JSON.stringify(tarefas))
+
+    ipt.value = ''
+
+});
+
+function criarTarefa(txt) {
     let novatarefa = document.createElement('li')
     let texto = document.createElement('span')
     let remove = document.createElement('button')
     let concluido = document.createElement('button')
     let editar = document.createElement('button')
-
+    
     texto.innerText = txt
     remove.innerText = "Excluir"
     concluido.innerText = "✓"
@@ -20,10 +30,7 @@ addbutton.addEventListener("click", function() {
     novatarefa.appendChild(concluido)
     novatarefa.appendChild(editar)
     novatarefa.appendChild(remove)
-
     ul.appendChild(novatarefa)
-
-    ipt.value = ''
 
     remove.addEventListener('click', function(){
         novatarefa.remove()
@@ -37,4 +44,5 @@ addbutton.addEventListener("click", function() {
         texto.contentEditable = 'true'
         texto.focus()
     });
-});
+
+}
